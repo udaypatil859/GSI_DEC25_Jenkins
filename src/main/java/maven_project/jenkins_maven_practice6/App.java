9306da79a35e395841f0872645f845d93515cff6
Uday Patil <udaypatil859@gmail.com>
2026-05-12 08:50:14 +0530

    initial changes

---
+++ src/main/java/maven_project/jenkins_maven_practice6/App.java
@@ -0,0 +1,13 @@
+package maven_project.jenkins_maven_practice6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/jenkins_maven_practice6/LoginTest.java
@@ -0,0 +1,11 @@
+package jenkins_maven_practice6;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+@Test
+	void hello()
+	{
+	System.out.println("****Login test is working****");
+	}
+}
